f96d8a0aabe270ea32d8e46c033a2a36cc78104f
anupam <anupamminz300am@gmail.com>
2026-06-04 20:27:45 +0530

    feat: blank window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,45 @@
 #include <cstdio>
 
+#include <glad/glad.h>
+///
+#include <GLFW/glfw3.h>
+
+#define SCREEN_WIDTH 1024
+#define SCREEN_HEIGHT 1024
+
 int main() {
-	printf("hello world\n");
+	if (!glfwInit()) {
+		perror("init glfw");
+		return -1;
+	}
+
+	// glfw window hints
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow *window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT,
+					      "triangle", NULL, NULL);
+	if (window == NULL) {
+		fprintf(stderr, "Failed to create glfw window\n");
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		fprintf(stderr, "Failed to init glad");
+		glfwTerminate();
+		return -1;
+	}
+
+	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
 	return 0;
 }
