3557330c7c2ff5322db17bd2d6203834179d87ad
anupam <anupamminz300am@gmail.com>
2026-06-06 10:57:11 +0530

    feat: red triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <cstdlib>
 
 #include <glad/glad.h>
 ///
@@ -6,6 +7,8 @@
 
 #define SCREEN_WIDTH 1024
 #define SCREEN_HEIGHT 1024
+#define LOG_BUF_S_MAX 1 << 13 // 8 KB
+#define FILE_RD_S_MAX 1 << 22 // 4 MB
 
 void GLAPIENTRY MessageCallback(GLenum source, GLenum type, GLuint id,
 				GLenum severity, GLsizei length,
@@ -16,8 +19,34 @@ void GLAPIENTRY MessageCallback(GLenum source, GLenum type, GLuint id,
 		severity, message);
 }
 
+size_t read_file(const char *path, const char **output) {
+	FILE *f = fopen(path, "r");
+	if (f == NULL) {
+		fprintf(stderr, "Failed to read file: File not found: %s",
+			path);
+		return 0;
+	}
+	fseek(f, 0, SEEK_END);
+	size_t file_s = ftell(f);
+	if (file_s+1 > FILE_RD_S_MAX) {
+		fprintf(stderr,
+			"Failed to read file: Max file size allowed exceeded: "
+			"Max File size %d, file path: %s",
+			FILE_RD_S_MAX, path);
+		return 0;
+	}
+	char *buf = (char *)malloc(file_s+1);
+	fseek(f, 0, SEEK_SET);
+	size_t bytes_read = fread(buf, file_s, 1, f);
+	fclose(f);
+	buf[file_s] = '\0';
+	*output = buf;
+	return bytes_read;
+}
+
 int main() {
-	uint32_t VAO, VBO;
+	unsigned int VAO, VBO;
+
 	if (!glfwInit()) {
 		perror("init glfw");
 		return -1;
@@ -51,24 +80,109 @@ int main() {
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
 	float triangle[] = {
-	    -1.0f, -1.0f, 0.0f, ///
-	    0.0f,  1.0f,  0.0f, ///
-	    1.0f,  -1.0f, 0.0f, ///
+	    -0.5f, -0.5f, 0.0f, ///
+	    0.0f,  0.5f,  0.0f, ///
+	    0.5f,  -0.5f, 0.0f, ///
 	};
 	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 9, triangle,
 		     GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void *)0);
+	const char *vertexShaderCode = NULL;
+	const char *fragmentShaderCode = NULL;
+	unsigned int vertexShaderID, fragmentShaderID, programID;
+	int shaderStatus = 0, programStatus = 0;
+	char *logBuffer = (char *)malloc(LOG_BUF_S_MAX);
+	int logBufferLen = 0;
+
+	if (read_file("shaders/basic/vertex.glsl", &vertexShaderCode) == 0) {
+		fprintf(stderr, "Failed to read vertex shader");
+		return -1;
+	}
+	if (read_file("shaders/basic/fragment.glsl", &fragmentShaderCode) ==
+	    0) {
+		fprintf(stderr, "Failed to read vertex shader");
+		return -1;
+	}
+
+	vertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShaderID, 1, &vertexShaderCode, NULL);
+	glCompileShader(vertexShaderID);
+	glGetShaderiv(vertexShaderID, GL_COMPILE_STATUS, &shaderStatus);
+	if (shaderStatus == GL_FALSE) {
+		char *logBuffer = (char *)malloc(LOG_BUF_S_MAX);
+		int logBufferLen = 0;
+		glGetShaderInfoLog(vertexShaderID, LOG_BUF_S_MAX, &logBufferLen,
+				   logBuffer);
+		if (logBufferLen == LOG_BUF_S_MAX) {
+			fprintf(stderr, "Failed to load output log: Log "
+					"buffer max size reached. The rest of "
+					"the log would be truncated\n");
+		} else {
+			logBuffer[logBufferLen] = '\0';
+		}
+		fprintf(stdout,
+			"**GL ERROR**\nVertex Shader Compilation Failed\n%s\n",
+			logBuffer);
+		return -1;
+	}
+
+	fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShaderID, 1, &fragmentShaderCode, NULL);
+	glCompileShader(fragmentShaderID);
+	glGetShaderiv(fragmentShaderID, GL_COMPILE_STATUS, &shaderStatus);
+	if (shaderStatus == GL_FALSE) {
+		glGetShaderInfoLog(fragmentShaderID, LOG_BUF_S_MAX,
+				   &logBufferLen, logBuffer);
+		if (logBufferLen == LOG_BUF_S_MAX) {
+			fprintf(stderr, "Failed to load output log: Log "
+					"buffer max size reached. The rest of "
+					"the log would be truncated\n");
+		} else {
+			logBuffer[logBufferLen] = '\0';
+		}
+		fprintf(
+		    stdout,
+		    "**GL ERROR**\nFragment Shader Compilation Failed\n%s\n",
+		    logBuffer);
+		return -1;
+	}
+
+	programID = glCreateProgram();
+	glAttachShader(programID, vertexShaderID);
+	glAttachShader(programID, fragmentShaderID);
+	glLinkProgram(programID);
+	glGetProgramiv(programID, GL_LINK_STATUS, &programStatus);
+	if (programStatus == GL_FALSE) {
+		glGetProgramInfoLog(programID, LOG_BUF_S_MAX, &logBufferLen,
+				    logBuffer);
+		if (logBufferLen == LOG_BUF_S_MAX) {
+			fprintf(stderr, "Failed to load output log: Log "
+					"buffer max size reached. The rest of "
+					"the log would be truncated\n");
+		} else {
+			logBuffer[logBufferLen] = '\0';
+		}
+		fprintf(stdout, "**GL ERROR**\nProgram Link Failed\n%s\n",
+			logBuffer);
+		return -1;
+	}
+	glDetachShader(programID, vertexShaderID);
+	glDetachShader(programID, fragmentShaderID);
+	glDeleteShader(vertexShaderID);
+	glDeleteShader(fragmentShaderID);
+	glUseProgram(programID);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
+			      (void *)0);
 	glEnableVertexAttribArray(0);
 
 	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
 		glClear(GL_COLOR_BUFFER_BIT);
-
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
-		glfwPollEvents();
 		glfwSwapBuffers(window);
 	}
 	glfwDestroyWindow(window);
